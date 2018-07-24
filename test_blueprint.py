a9dcbb1ea34f64ed73ffdd3b01dba9aee6ee7582
Dmitriy Andrushko <dmytro.andrushko@gmail.com>
2018-07-24 17:29:02 +0300

    Blueprint with 2 VNFs fixed and appropriate test was added

---
+++ test_blueprint.py
@@ -0,0 +1,11 @@
+from cloudify.test_utils import workflow_test
+import unittest
+
+
+class TestBlueprint(unittest.TestCase):
+
+    @workflow_test('5G-T-Cloudify-demo.yaml')
+    def test_blueprint(self, cfy_local):
+        cfy_local.execute('install', task_retries=1)
+        raw_input()
+        cfy_local.execute('uninstall', task_retries=1)
